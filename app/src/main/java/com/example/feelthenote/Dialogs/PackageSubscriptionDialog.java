1f761c12b4ff8be1c6565376b3d06b715e707ba1
Nisarg851 <nisargmahyavanshi8501@gmail.com>
2022-02-27 15:50:05 +0530

    Package Subscription Dialog

---
+++ app/src/main/java/com/example/feelthenote/Dialogs/PackageSubscriptionDialog.java
@@ -0,0 +1,12 @@
+package com.example.feelthenote.Dialogs;
+
+import android.app.Dialog;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+public class PackageSubscriptionDialog extends AppCompatDialogFragment {
+
+}
